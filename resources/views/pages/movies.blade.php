df95dd41324d64fbe344c2387ce5983ef5f4c08a
Luis-Mocha <luismocha97@gmail.com>
2023-06-12 01:58:54 +0200

    stampo i film

--- resources/views/pages/movies.blade.php
+++ resources/views/pages/movies.blade.php
@@ -9,6 +9,32 @@ ModelContoller | Movies
     <div class="text-center p-5">
         <h1>Pagina Movies</h1>
     </div>
+
+    <div class="container mb-5">
+        <div class="d-flex flex-wrap gap-3 justify-content-evenly">
+
+            @foreach($movies as $elem)
+            <div class="card col-5 p-2">
+                <span class="fst-italic">
+                    Movie #{{$elem['id']}}
+                </span>
+
+                <h2 class="text-center my-4">
+                    {{$elem['title']}}
+                </h2>
+
+                <div class="d-flex justify-content-evenly">
+                    <span>Data di uscita: {{$elem['date']}}</span>
+                    <span>Voto: {{$elem['vote']}}</span>
+                </div>
+            </div>
+            @endforeach
+
+        </div>
+       
+        
+
+    </div>
     
 
 @endsection
