6d35d03c8e3a5111213d1e8ab896e1fe4604e752
Luis-Mocha <luismocha97@gmail.com>
2023-06-12 00:22:12 +0200

    creazione finzione cel controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function getMovies() {
+        // echo 'heheheh';
+        return view('pages.home');
+    }
 }
