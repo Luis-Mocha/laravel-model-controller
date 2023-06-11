1484283292329737f3db77f541ed59fba86b3159
Luis-Mocha <luismocha97@gmail.com>
2023-06-12 01:44:00 +0200

    richiamo i dati dal database

--- app/Http/Controllers/Guest/MoviesController.php
+++ app/Http/Controllers/Guest/MoviesController.php
@@ -10,8 +10,8 @@ class MoviesController extends Controller
 {
     public function getMovies() {
 
-        
+        $movies = Movie::All();  //è uguale a " SELECT * FROM `books` "
 
-        return view('pages.movies');
+        return view('pages.movies', compact ('movies'));
     }
 }
