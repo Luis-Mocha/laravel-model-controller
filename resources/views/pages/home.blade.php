eacf9d224e5ade3e995d92e834699591753f37ca
Luis-Mocha <luismocha97@gmail.com>
2023-06-11 23:22:04 +0200

    file home.blade

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+@extends( 'layout.app' )
+
+@section ('title-page')
+ModelContoller | Home
+@endsection
+
+@section( 'content-main' )
+
+    <div class="text-center p-5">
+        <h1>Pagina Home</h1>
+    </div>
+    
+
+@endsection
